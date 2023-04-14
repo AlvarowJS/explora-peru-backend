fbcea1396e89b07020b6466126604d22d2db8528
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2023-04-14 17:36:57 -0500

    avance tours

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -9,14 +9,35 @@ class TourController extends Controller
 {
     public function index()
     {
-        $tours = Tour::all();
+        // $tours = Tour::all();
+        // return response()->json($tours);
+        $tours = Tour::with('lugare')->get();
+
         return response()->json($tours);
     }
 
     public function store(Request $request)
     {
-        $tour = Tour::create($request->all());
-        return response()->json($tour, 201);
+   
+        $tour = new Tour();
+        $tour->titulo = $request->input('titulo');
+        $tour->descripcion_spanish = $request->input('descripcion_spanish');
+        $tour->descripcion_english = $request->input('descripcion_english');
+        $tour->incluye_spanish = $request->input('incluye_spanish');
+        $tour->incluye_english = $request->input('incluye_english');
+        $tour->duracion = $request->input('duracion');
+        $tour->lugare_id = $request->input('lugare_id');
+
+        if ($request->hasFile('img')) {
+            $file = $request->file('img');
+            $fileName = uniqid() . '.' .$file->getClientOriginalName();
+            $path = $file->storeAs('public/tours', $fileName);
+            $tour->img = $fileName;
+        }
+
+        $tour->save();
+        return response()->json($tour);
+        
     }
 
     public function show($id)
