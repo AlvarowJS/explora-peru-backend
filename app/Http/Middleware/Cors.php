5ec243a206eca24e916ed0317283ad01507f5e61
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2023-05-07 12:20:56 -0500

    cors especificos

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', 'https://agentes.peruexploring.pe');
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:5173/');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
 
