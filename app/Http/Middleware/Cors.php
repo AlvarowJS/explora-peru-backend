5c43eba071b480f0cddcc918289e8abd15f7a7cf
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2023-05-06 23:34:25 -0500

    solucion cors con middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
 
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
 
         return $response;
     }
